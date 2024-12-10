dbf92fc4cbda9bd1f56c5514ad47f30336bcb3bf
mdoradom <mariodoradomartinez@gmail.com>
2024-12-10 23:33:01 +0100

    bomba

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "https://mdoradom.com/SecondBrain",
+    baseUrl: "/SecondBrain/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
