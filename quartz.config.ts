d132100a8c1d07164665a7b631a62823cbb0fdd2
mdoradom <mariodoradomartinez@gmail.com>
2024-12-11 00:54:48 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "/SecondBrain/",
+    baseUrl: "http://secondbrain.mdoradom.com/",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
